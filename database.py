2e469b881156b3c43739acaef7b5f297b4a4faaa
Anthony O <anthony@gcch.se>
2026-08-07 13:03:01 +0200

    databse envs

--- database.py
+++ database.py
@@ -1,9 +1,25 @@
 import os
 
+from psycopg.conninfo import make_conninfo
 from psycopg_pool import ConnectionPool
 
 
-connection_string = os.environ["POSTGRES_CONNECTION_STRING"]
+DB_HOST = os.environ["DB_HOST"]
+DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_NAME = os.environ["DB_NAME"]
+DB_USER = os.environ["DB_USER"]
+DB_PASSWORD = os.environ["DB_PASSWORD"]
+DB_SSLMODE = os.environ.get("DB_SSLMODE", "require")
+
+
+connection_string = make_conninfo(
+    host=DB_HOST,
+    port=DB_PORT,
+    dbname=DB_NAME,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    sslmode=DB_SSLMODE,
+)
 
 
 pool = ConnectionPool(
